eab4476b7685895b4b2779d482ff2a34f8b069bb
Gabriel Carvalho <gribeirocarvalho@hotmail.com>
2023-08-24 22:33:52 -0300

    Finalização do Projeto

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Retangulo
+{
+  public double Largura {get; set;}
+  public double Altura {get; set;}
+
+  public Retangulo(double largura, double altura)
+  {
+    Largura = largura;
+    Altura = altura;
+  }
+
+  public double CalcularArea()
+  {
+    return Largura * Altura;
+  }
+
+  public double CalcularPerimetro()
+  {
+    return 2 * (Largura + Altura);
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Write("Informe a Largura do retângulo: ");
+    double largura = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Informe a Altura do retêngulo: ");
+    double altura = Convert.ToDouble(Console.ReadLine());
+
+    Retangulo retangulo  = new Retangulo(largura, altura);
+
+    double area = retangulo.CalcularArea();
+    double perimetro = retangulo.CalcularPerimetro();
+
+    Console.WriteLine($"Área do Retângulo: {area}");
+    Console.WriteLine($"Perímetro do Retângulo: {perimetro}");
+  }
+}
---
+++ instrucoes.md
@@ -0,0 +1,16 @@
+# Teste de Performance 2
+## Exercício #3
+
+Implemente um programa no qual o usuário informe tamanho de largura e altura de um retângulo, e em seguida seja exibido o valor de sua área e perímetro.
+
+Requisitos:
+
+1. Regra de cálculo da área: largura * altura;
+2. Regra de cálculo do perímetro: 2 * (largura + altura);
+
+Requisitos técnicos:
+
+1. Use uma classe para representar o retângulo;
+2. Crei o construtor na classe (item 1) para definir a largura e altura;
+3. Crie métodos na classe (item 1) para calcular cada regra;
+4. Utilize a classe Console apenas no programa principal;
